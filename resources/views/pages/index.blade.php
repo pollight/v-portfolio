30f1112344688369f2d1de2f5de9341e9047147d
Pavel Zholnerovich <pavel.zholnerovich@yandex.ru>
2021-01-29 01:12:53 +0300

    header

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,12 +3,46 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <title>Laravel</title>
     </head>
     <body>
         <div id="app" class="app">
-                home page
+            <header>
+                <div class="header__logo logo">
+                    Вельяминов Г.М.
+                </div>
+
+                <div class="header__nav nav">
+                    <div class="nav__item">
+                        <div class="nav__link nav__link_active">
+                            <a href="#">Главная</a>
+                        </div>
+                    </div>
+                    <div class="nav__item">
+                        <div class="nav__link nav__link_active">
+                            <a href="#">Информация</a>
+                        </div>
+                    </div>
+                    <div class="nav__item">
+                        <div class="nav__link nav__link_active">
+                            <a href="#">Работы</a>
+                        </div>
+                    </div>
+                    <div class="nav__item">
+                        <div class="nav__link nav__link_active">
+                            <a href="#">Контакты</a>
+                        </div>
+                    </div>
+                    <div class="nav__item">
+                        <div class="nav__link nav-local">
+                            <a href="#" class="nav-local__ru">Ru</a>
+                            <span>/</span>
+                            <a href="#" class="nav-local__en">En</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
         </div>
     </body>
 </html>
