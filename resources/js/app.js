f427311daf64e393c5ad414b881536b99ab73423
Pavel Zholnerovich <pavel.zholnerovich@yandex.ru>
2021-01-30 18:00:18 +0300

    desc smock

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import $ from "jquery";
+
+import 'owl.carousel/dist/assets/owl.carousel.css';
+require('owl.carousel');
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+    });
+});
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,48 +1,348 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <title>Laravel</title>
-    </head>
-    <body>
-        <div id="app" class="app">
-            <header>
-                <div class="header__logo logo">
-                    Вельяминов Г.М.
-                </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Laravel</title>
+</head>
+<body>
+<div id="app" class="app">
+    <header class="header">
+        <div class="header__container container">
+            <div class="header__logo logo">
+                Вельяминов Г.М.
+            </div>
 
-                <div class="header__nav nav">
-                    <div class="nav__item">
-                        <div class="nav__link nav__link_active">
-                            <a href="#">Главная</a>
-                        </div>
-                    </div>
-                    <div class="nav__item">
-                        <div class="nav__link nav__link_active">
-                            <a href="#">Информация</a>
-                        </div>
-                    </div>
-                    <div class="nav__item">
-                        <div class="nav__link nav__link_active">
-                            <a href="#">Работы</a>
-                        </div>
-                    </div>
-                    <div class="nav__item">
-                        <div class="nav__link nav__link_active">
-                            <a href="#">Контакты</a>
-                        </div>
-                    </div>
-                    <div class="nav__item">
-                        <div class="nav__link nav-local">
-                            <a href="#" class="nav-local__ru">Ru</a>
-                            <span>/</span>
-                            <a href="#" class="nav-local__en">En</a>
-                        </div>
+            <div class="header__nav nav">
+                <div class="nav__item">
+                    <a href="#" class="nav__link nav__link_active">Главная</a>
+                </div>
+                <div class="nav__item">
+                    <a href="#" class="nav__link">Информация</a>
+                </div>
+                <div class="nav__item">
+                    <a href="#" class="nav__link">Работы</a>
+                </div>
+                <div class="nav__item">
+                    <a href="#" class="nav__link">Контакты</a>
+                </div>
+                <div class="nav__item">
+                    <div class="nav-local">
+                        <a href="#" class="nav-local__link nav__link nav__link_active">Ru</a>
+                        <span>/</span>
+                        <a href="#" class="nav-local__link nav__link">En</a>
                     </div>
                 </div>
-            </header>
+            </div>
         </div>
-    </body>
+    </header>
+
+    <div class="content">
+        <div class="about">
+            <img src="/img/photo.png" class="about__photo" alt="">
+
+            <div class="about__container container">
+                <div class="about__text">
+                    <h1 class="about__title title">
+                        Georgy Michailovich Velyaminov
+                    </h1>
+                    <h2 class="about__sub-title">
+                        The title
+                    </h2>
+
+                    <p class="about__description">
+                        This is Photoshop's version of Lorem Ipsum.
+                        Proin gravida nibh vel velit auctor aliquet.
+                        Aenean sollicitudin, lorem quis bibendum
+                    </p>
+
+                    <button class="about__button button">Информация</button>
+                </div>
+            </div>
+
+            <div class="about__arrow arrow arrow_down-white"></div>
+        </div>
+
+        <div class="information">
+            <div class="information__text container">
+                <h2 class="information__title title">
+                    About Georgiy
+                </h2>
+
+                <p class="information__description">
+                    TThis is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean
+                    sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.
+                    Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec
+                    tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit.
+                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris
+                    in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed
+                    ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat,
+                    velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim. ornare odio.
+                    Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora
+                    torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis
+                    dapibus condimentum sit amet a augue.
+                </p>
+            </div>
+
+            <div class="information__arrow arrow arrow_down-white"></div>
+        </div>
+
+        <div class="list-works">
+            <div class="list-works__text container">
+                <h2 class="list-works__title title">
+                    Перечень работ
+                </h2>
+
+                <div class="list-works__slider slider">
+                    <div class="owl-carousel">
+                        <ul class="slider__item">
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                        </ul>
+
+                        <ul class="slider__item">
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                        </ul>
+
+                        <ul class="slider__item">
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                            <li>
+                                <div class="slider__title">
+                                    Год: 1989
+                                </div>
+                                <div class="slider__description">
+                                    Право международное и национальное
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="footer__container container">
+            <div class="footer__copyright">
+                © 2018 Designed by Fedor Velyaminov
+            </div>
+
+            <div class="footer__name">
+                Вельяминов Г. М:
+            </div>
+            <div class="footer__contacts">
+                <a href="tel:+79098657543" class="footer__phone">+7 909 865 75 43</a>
+                <a href="mailto:Velyaminov@mail.ru" class="footer__mail">Velyaminov@mail.ru</a>
+            </div>
+        </div>
+    </footer>
+</div>
+
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
 </html>
