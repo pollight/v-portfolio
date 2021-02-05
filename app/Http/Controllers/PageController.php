e6c667cafd32dc8c5ed7d9fc6fdd20b1c71132cd
Pavel Zholnerovich <pavel.zholnerovich@yandex.ru>
2021-02-05 18:38:41 +0300

    fix

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,17 +8,17 @@ class PageController extends Controller
 {
     public function index($locale = "ru")
     {
-        if (!$locale)
-            $locale = 'ru';
-
-        if(!in_array($locale, ["ru", "en"]))
-            return redirect()->to("/");
-
-        app()->setLocale($locale);
-
-        $content = Content::withTranslation($locale)->get();
-        dd($content);
-        $data['content'] = $content;
+//        if (!$locale)
+//            $locale = 'ru';
+//
+//        if(!in_array($locale, ["ru", "en"]))
+//            return redirect()->to("/");
+//
+//        app()->setLocale($locale);
+//
+//        $content = Content::withTranslation($locale)->get();
+//        dd($content);
+//        $data['content'] = $content;
         return view("pages.index", $data);
     }
 }
