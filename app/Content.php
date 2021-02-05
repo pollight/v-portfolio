ae72342317fc3248842b49bef04aa93f55bba57f
Pavel Zholnerovich <pavel.zholnerovich@yandex.ru>
2021-02-05 22:55:01 +0300

    adminka bd

---
+++ app/Content.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+class Content extends Model
+{
+    use Translatable;
+
+    protected $translatable = ['value'];
+}
---
+++ app/Work.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+
+class Work extends Model
+{
+    use Translatable;
+
+    protected $translatable = ['name', 'description'];
+}
